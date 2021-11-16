455753d363bdc846d339471b0e4e27b69ff68b7a
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-11-16 22:47:53 +0100

    explored model objects methods like filter,get, and all

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 from . import views
+from .views import Hello
 
 urlpatterns = [
     #pointing our app to a view as defined in views.py
     path('', views.welcome),
+    path('test/', Hello.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,26 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from .models import Contact
 
 # Create your views here.
 
 def welcome(request):
+    """ A function based view"""
     return HttpResponse('Welcome to our first views from django without DRF')
+
+
+class Hello(View):
+    """ A class based view"""
+    #get all the contact in database
+    contacts = Contact.objects.all()
+    # contacts = Contact.objects.filter(property=True)=> returns all contact meeting criterion
+    # contacts = Contact.objects.get()=> returns only one contact with an attribute 
+    #loop through the array of contacts
+    for contact in contacts:
+        message = f"You have {contact.name} in your Cirkle"
+    
+
+    def get(self, request):
+        
+        return HttpResponse(self.message)
