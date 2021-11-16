1abc34ab737f21cdb010c02d87cdab3cd571d4a3
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-11-16 21:45:57 +0100

    created app and contact model

---
+++ api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+    """Class defines a contact object within the database"""
+
+    RELATIONSHIP = (
+        ("Friend", "a friend"),
+        ("Family", "met as family"),
+        ("Office", "met at the office"),
+        ("Stranger", "A stranger")
+
+    )
+    #A contact should have a name that is never blank and never unique as more than one person can bear one name
+    name = models.CharField(max_length=40,  blank=False, unique=False) 
+    phone_number = models.CharField(max_length=14, unique=True, blank=False)
+    email = models.EmailField(max_length=100, unique=True, blank=True)
+    address = models.TextField(max_length=200, unique=False, blank=True)
+    #Every contact has a relationship for the user based on the choice listings
+    relation = models.CharField(max_length=50, blank=False, choices= RELATIONSHIP)
+    created_on = models.DateTimeField( null=True, auto_now_add=True)
+    image = models.ImageField(upload_to="images/", blank=True)
+    
+
+
+
