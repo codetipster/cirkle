d8a7cee5afe3bb78a396498b1eda25ee12b13846
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-11-21 23:54:28 +0100

    added swagger for documentation

--- base/urls.py
+++ base/urls.py
@@ -2,10 +2,30 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="MYX API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.ourapp.com/policies/terms/",
+      contact=openapi.Contact(email="contact@myxapp.local"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     #pointing django to our application urls in api
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('auth/', obtain_auth_token),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/api.json/', schema_view.without_ui( cache_timeout=0),name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
